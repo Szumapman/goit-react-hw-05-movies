e6a68fcf7c1bb4ad8be8783e4a0f1e58c392bf53
Paweł Szumański <pawel.szumanski@outlook.com>
2025-02-17 18:45:27 +0100

    Update Movies.tsx

--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -1,5 +1,72 @@
-export const Movies = () => {
+import { useState, useEffect } from "react";
+import { fetchData } from "../utils/TmdbAPITools";
+import { MOVIES_SEARCH } from "../constants/APILinks";
+import { useLocation, useSearchParams } from "react-router-dom";
+import { createMovieListItems } from "../utils/CreateMovieListItems";
+import useScrollToHash from "../hooks/useScrollToHash";
+
+const Movies = () => {
+    const [query, setQuery] = useState('');
+    const [movies, setMovies] = useState([]);
+    const [error, setError] = useState(false);
+    const [searchParams, setSearchParams] = useSearchParams();
+    const movie = searchParams.get('movie');
+    const location = useLocation();
+
+    useScrollToHash();
+    
+    const handleQuery = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setQuery(event.target.value);
+    };
+
+    useEffect(() => {
+        if (movie) {
+            fetchData(MOVIES_SEARCH, movie).then(response => {
+                setMovies(response.results);
+            }).catch(error => {
+                setError(error.message);
+            });
+        }
+    }, [movie]);
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const form = event.currentTarget;
+        if (query.trim() === '') {
+            return;
+        }
+        setSearchParams({ movie: query });
+        fetchData(MOVIES_SEARCH, query).then(response => {
+            setMovies(response.results);
+        }).catch(error => {
+            setError(error.message);
+        });
+        form.reset();
+    };
+
     return (
-        <h1>Movies</h1>
+        <>
+            <form  onSubmit={handleSubmit} id="search-form">
+                <button type="submit">
+                    <span>Search</span>
+                </button>
+
+                <input
+                    type="text"
+                    autoComplete="off"
+                    autoFocus
+                    placeholder="Search movies"
+                    onChange={handleQuery}
+                />
+            </form>
+            {error && <p>{error}</p>}
+            {!error &&
+                <ul>
+                    {createMovieListItems(movies, location)}
+                </ul>
+            }
+        </>
     );
 }
+
+export default Movies;
