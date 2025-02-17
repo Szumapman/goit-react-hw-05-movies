f69a310482f914575cc70cb60a24fb7053bbf32d
Paweł Szumański <pawel.szumanski@outlook.com>
2025-02-17 18:44:56 +0100

    Create useScrollToHash.ts

---
+++ src/hooks/useScrollToHash.ts
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+const useScrollToHash = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.hash) {
+            setTimeout(() => {
+                const element = document.getElementById(location.hash.slice(1));
+                if (element) {
+                    element.scrollIntoView({ behavior: "instant" });
+                }
+            }, 200);
+        }
+    }, [location.hash]);
+};
+
+export default useScrollToHash;
