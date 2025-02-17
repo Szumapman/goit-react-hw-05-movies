e0b1ca0d7ae7ce48b553695782016561298a669f
Paweł Szumański <pawel.szumanski@outlook.com>
2025-02-17 18:44:49 +0100

    Update APILinks.ts

--- src/constants/APILinks.ts
+++ src/constants/APILinks.ts
@@ -1,1 +1,9 @@
+export const BASE_TMDB_API_URL = "https://api.themoviedb.org/3/";
+
 export const MOVIES_TRENDING_DAILY = "trending/movie/day";
+
+export const MOVIES_SEARCH = "search/movie";
+
+export const MOVIE = "movie/";
+
+export const BASE_POSTER_PATH = "https://image.tmdb.org/t/p/w300/";
