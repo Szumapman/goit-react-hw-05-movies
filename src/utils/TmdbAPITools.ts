877395594ec1352984af0341bf81f1ac770cd779
Paweł Szumański <pawel.szumanski@outlook.com>
2025-02-17 18:45:47 +0100

    Update TmdbAPITools.ts

--- src/utils/TmdbAPITools.ts
+++ src/utils/TmdbAPITools.ts
@@ -1,14 +1,15 @@
 import axios from "axios";
+import { BASE_TMDB_API_URL } from "../constants/APILinks";
 
 const TMDB_API_KEY = "941b4060b32a2bcfaa35765cb3db42d2";
 
-axios.defaults.baseURL = "https://api.themoviedb.org/3/";
+axios.defaults.baseURL = BASE_TMDB_API_URL;
 axios.defaults.params = {
     api_key: TMDB_API_KEY,
     language: "en-US",
 };
 
-export const fetchData = async (url: string) => {
-    const { data } = await axios.get(url);
+export const fetchData = async (url: string, query?: string) => {
+    const { data } = await axios.get(url, query ? { params: { query } } : {});
     return data;
 };
