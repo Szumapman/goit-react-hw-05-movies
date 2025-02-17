e90307976e39f057b636cc77e81fe2dc381a9b73
Paweł Szumański <pawel.szumanski@outlook.com>
2025-02-17 18:44:37 +0100

    add lazy loading

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,22 @@
 import { Route, Routes } from 'react-router-dom'
+import { lazy, Suspense } from 'react'
 import './App.css'
 import { SharedLayout } from './SharedLayout'
-import { Home } from '../pages/Home'
-import { Movies } from '../pages/Movies';
-import { MovieDetails } from '../pages/MovieDetails';
+const Home = lazy(() => import('../pages/Home'));
+const Movies = lazy(() => import('../pages/Movies'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails'));
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<SharedLayout />}>
-        <Route index element={<Home />} />
-        <Route path='movies' element={<Movies />} />
-        <Route path="movies/:movieId" element={<MovieDetails />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path='/' element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path='movies' element={<Movies />} />
+          <Route path="movies/:movieId" element={<MovieDetails />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
