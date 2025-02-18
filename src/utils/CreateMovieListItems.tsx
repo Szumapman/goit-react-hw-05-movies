59f8890ad12a2cdeedca8332b44a5209ebc86cd5
Paweł Szumański <pawel.szumanski@outlook.com>
2025-02-18 17:32:25 +0100

    Update img src

--- src/utils/CreateMovieListItems.tsx
+++ src/utils/CreateMovieListItems.tsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 import { Movie } from "../interfaces/Movie";
 import noPoster from "../assets/images/no_poster.jpg";
+import { PosterURL } from "./APILinks";
 
 export const createMovieListItems = (movies: Movie[], location: any) => {
     return movies.map(({ id, title, poster_path }) => (
         <li key={id} id={String(id)}>
             <Link to={`/movies/${id}#${id}`} state={{ from: { ...location, hash: `#${id}` } }}>
                 <div>
-                    <img src={poster_path ? `https://image.tmdb.org/t/p/w300/${poster_path}` : noPoster} alt={`${title} poster`} />
+                    <img src={poster_path ? PosterURL(poster_path) : noPoster} alt={poster_path ? `${title} poster` : `replacement poster for ${title}`} />
                     <p>{title}</p>
                 </div>
             </Link>
