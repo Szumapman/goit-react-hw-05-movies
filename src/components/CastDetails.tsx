94910f41aacbe070a9e5b041f10308089ca9e8f2
Paweł Szumański <pawel.szumanski@outlook.com>
2025-02-18 17:30:56 +0100

    Create CastDetails.tsx

---
+++ src/components/CastDetails.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchData } from "../utils/TmdbAPITools";
+import { Cast } from "../interfaces/Cast";
+import { MovieCreditsURL } from "../utils/APILinks";
+import { CastPhotoURL } from "../utils/APILinks";
+import noPhoto from "../assets/images/no_photo.jpg";
+
+const CastDetails = () => {
+    const [cast, setCast] = useState<Cast[]>([]);
+    const { movieId } = useParams();
+
+    useEffect(() => {
+        fetchData(MovieCreditsURL(movieId)).then(response => {
+            setCast(response.cast);
+        }).catch(error => {
+            console.log(error);
+        })
+    }, [movieId]);
+
+    return (
+        <>
+            <ul>
+                {cast.map(({id, profile_path, name, character}) => (
+                    <li key={id}>
+                        <img src={profile_path ? CastPhotoURL(profile_path) : noPhoto} alt={profile_path ? `${name} photo` : `replacement image for ${name}`} />
+                        <p>{name}</p>
+                        <p>Character: {character}</p>
+                    </li>
+                ))}
+            </ul>
+        </>
+    )
+};
+
+export default CastDetails;
