7a6c86ec295edef1d65ee3a3dec498b480b89b73
Paweł Szumański <pawel.szumanski@outlook.com>
2025-02-19 18:14:36 +0100

    Update Reviews.tsx

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,3 +1,32 @@
-const Review = () => <div>Reviews</div>;
+import { useParams } from "react-router-dom";
+import { MovieReviewsURL } from "../utils/APILinks";
+import { fetchData } from "../utils/TmdbAPITools";
+import { useEffect, useState } from "react";
+import { ReviewInterface } from "../interfaces/ReviewInterface";
+
+const Review = () => {
+    const [reviews, setReviews] = useState<ReviewInterface[]>([]);
+    const { movieId } = useParams();
+
+    useEffect(() => {
+        fetchData(MovieReviewsURL(movieId)).then(response => {
+            console.log(response);
+            setReviews(response.results);
+        }).catch(error => {
+            console.log(error);
+        });
+    }, [movieId]);
+
+    return (
+        <ul>
+            {reviews.map(review => (
+                <li key={review.id}>
+                    <h3>{review.author}</h3>
+                    <p>{review.content}</p>
+                </li>
+            ))}
+        </ul>
+    );
+};
 
 export default Review;
