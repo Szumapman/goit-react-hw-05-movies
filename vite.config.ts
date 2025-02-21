037278316a18dcbec38e0cd7d88d063839afb66d
Paweł Szumański <pawel.szumanski@outlook.com>
2025-02-21 08:12:47 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/goit-react-hw-05-movies/',
+  // base: '/goit-react-hw-05-movies/',
   plugins: [react()],
 })
