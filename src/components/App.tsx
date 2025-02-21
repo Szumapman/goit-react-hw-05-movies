691c5dd959eadb7b48e5a2e24e24c05f1b934706
Paweł Szumański <pawel.szumanski@outlook.com>
2025-02-21 11:59:15 +0100

    add page not found

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,11 +4,13 @@ import './App.css'
 import { SharedLayout } from './SharedLayout'
 import BASE_PATH from '../constants/BASE_PATH'
 
+
 const Home = lazy(() => import('../pages/Home'));
 const Movies = lazy(() => import('../pages/Movies'));
 const MovieDetails = lazy(() => import('../pages/MovieDetails'));
 const CastDetails = lazy(() => import('./CastDetails'));
 const Review = lazy(() => import('./Reviews'));
+const PageNotFound = lazy(() => import('../pages/PageNotFound'));
 
 export const App = () => {
   return (
@@ -20,6 +22,7 @@ export const App = () => {
             <Route path="cast" element={<CastDetails />} />
             <Route path="reviews" element={<Review />} />
           </Route>
+          <Route path="*" element={<PageNotFound />} />
         </Route>
       </Routes>
   );
---
+++ src/pages/PageNotFound.tsx
@@ -0,0 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import BASE_PATH from "../constants/BASE_PATH";
+
+const PageNotFound = () => {
+    const [counter, setCounter] = useState(5);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCounter(prevCounter => {
+                if (prevCounter === 1) {
+                    clearInterval(interval);
+                    navigate(BASE_PATH);
+                }
+                return prevCounter - 1;
+            });
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [navigate]);
+
+    return (
+        <>
+            <p>404 Page not found</p>
+            <p>Redirecting to main page in {counter} {counter === 1 ? "second" : "seconds..."}</p>
+        </>
+    )
+};
+
+export default PageNotFound
