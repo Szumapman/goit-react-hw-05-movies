f409cbb2c3641ac486b0644f05ceabf33b5a77f9
Paweł Szumański <pawel.szumanski@outlook.com>
2025-02-21 15:29:44 +0100

    delete navigate

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,16 +1,16 @@
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 const PageNotFound = () => {
     const [counter, setCounter] = useState(5);
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
     useEffect(() => {
         const interval = setInterval(() => {
             setCounter(prevCounter => {
                 if (prevCounter === 1) {
                     clearInterval(interval);
-                    navigate("/");
+                    // navigate("/");
                 }
                 return prevCounter - 1;
             });
